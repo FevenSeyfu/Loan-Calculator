5e6b764b007433cf487ef6654406b1c87dfc2c73
fevseyfu <66014053+fevseyfu@users.noreply.github.com>
2022-07-06 16:54:12 +0300

    compeleted

--- app.js
+++ app.js
@@ -11,10 +11,17 @@ var totalInterest = document.getElementById('total-interest');
 loadEventListeners();
 
 function loadEventListeners(){
-    inputForm.addEventListener('submit',calculateLoan);
+    inputForm.addEventListener('submit',function(e){
+        document.getElementById('results').style.display = 'none';
+        document.getElementById('loading').style.display = 'block';
+
+        setTimeout(calculateLoan,2000);
+
+        e.preventDefault();
+    });
 }
 
-function calculateLoan(e){
+function calculateLoan(){
     var n;
     var i;
     n= years.value * 12;
@@ -22,19 +29,49 @@ function calculateLoan(e){
     // console.log('n='+ n +',i='+ i)
     // Monthly payment calculation
     let z= Math.pow((1+i),n);
-    var monthly_payment =((amount.value)*i*z)/(z-1);
-    // console.log(monthly_payment);
+    var monthly_payment =(((amount.value)*i*z)/(z-1)).toFixed(2);
+    
+    
+    var total_payment =(  monthly_payment * years.value *12).toFixed(2);
 
-    var total_payment =  monthly_payment * years.value *12;
-    // console.log(total_payment);
-
-    var total_interest = total_payment - (amount.value)
+    var total_interest =( total_payment - (amount.value)).toFixed(2);
 
     // RESULTS
-    monthlyPayment.placeholder = monthly_payment;
-    totalPayment.placeholder = total_payment;
-    totalInterest.placeholder = total_interest;
+    if(isFinite(monthly_payment)){
+        monthlyPayment.placeholder = monthly_payment;
+        totalPayment.placeholder = total_payment;
+        totalInterest.placeholder = total_interest;
+        //  SHOW RESULTS
+        document.getElementById('results').style.display = 'block';
+        // HIDE LOADING
+        document.getElementById('loading').style.display = 'none';
+    }else{
+        showError('Please check your numbers.');
+    }
+    
   
-    e.preventDefault();
+    
 
+}
+
+function showError(error){
+
+    //  HIDE RESULTS
+    document.getElementById('results').style.display = 'none';
+    // HIDE LOADING
+    document.getElementById('loading').style.display = 'none';
+
+    const card =document.querySelector('.card');
+    const heading =document.querySelector('.heading');
+    
+    const errorDiv = document.createElement('div');
+    errorDiv.className = 'alert alert-danger';
+
+    errorDiv.appendChild(document.createTextNode(error));
+    card.insertBefore(errorDiv,heading)
+
+    setTimeout(clearError,3000);
+}
+function clearError(){
+    document.querySelector('.alert').remove();
 }
